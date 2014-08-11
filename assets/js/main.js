6679d940dc693d6aee06906dbbaa1c771095e5cc
ftroitsky <fedor.troitsky@gmail.com>
2014-08-11 13:40:08 +0400

    added jquery cookie

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,14 @@
  * Created by ftroitsky on 18/07/14.
  */
 
-$('.t8s-lead-form__button').click(
-    function(e){
-        e.preventDefault();
-        var btn = $(this);
+$('.t8s-lead-form').on('submit', function(event){
+        event.preventDefault();
+        var btn = $(this).find('.t8s-lead-form__button');
         btn.button('loading'),
             setTimeout(function () {
-                btn.button("reset")
+                btn.button("reset");
+                $('#successModal').modal('toggle');
+                console.log('modal');
             }, 3e3)
 //        $.ajax(
 //
